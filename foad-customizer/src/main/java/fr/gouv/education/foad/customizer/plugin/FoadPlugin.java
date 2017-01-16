ab10d8c2d8016047ca703b9275e20e41ec2d8db5
Cédric Krommenhoek <ckrommenhoek@osivia.com>
2017-01-16 15:55:19 +0000

    Suppression du service "Forums".

--- foad-customizer/src/main/java/fr/gouv/education/foad/customizer/plugin/FoadPlugin.java
+++ foad-customizer/src/main/java/fr/gouv/education/foad/customizer/plugin/FoadPlugin.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 import org.osivia.portal.api.customization.CustomizationContext;
 import org.osivia.portal.api.menubar.MenubarModule;
-import org.osivia.portal.api.taskbar.TaskbarFactory;
-import org.osivia.portal.api.taskbar.TaskbarItem;
-import org.osivia.portal.api.taskbar.TaskbarItems;
 
 import fr.gouv.education.foad.customizer.plugin.menubar.FoadMenubarModule;
 import fr.toutatice.portail.cms.nuxeo.api.domain.AbstractPluginPortlet;
@@ -47,8 +44,6 @@ public class FoadPlugin extends AbstractPluginPortlet {
     protected void customizeCMSProperties(String customizationID, CustomizationContext context) {
         // Customize menubar modules
         this.customizeMenubarModules(context);
-        // Taskbar items
-        this.customizeTaskbarItems(context);
     }
 
 
@@ -64,23 +59,6 @@ public class FoadPlugin extends AbstractPluginPortlet {
         // FOAD menubar module
         MenubarModule foadModule = new FoadMenubarModule();
         modules.add(foadModule);
-    }
-
-
-    /**
-     * Customize taskbar items.
-     *
-     * @param context customization context
-     */
-    private void customizeTaskbarItems(CustomizationContext context) {
-        // Taskbar items
-        TaskbarItems items = this.getTaskbarItems(context);
-        // Factory
-        TaskbarFactory factory = this.getTaskbarService().getFactory();
-
-        // Forums
-        TaskbarItem forums = factory.createCmsTaskbarItem("FORUMS", "FORUMS_TASK", "glyphicons glyphicons-conversation", "Folder");
-        items.add(forums);
     }
 
 }
